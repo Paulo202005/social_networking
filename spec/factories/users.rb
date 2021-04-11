e87638905563a7f43245f5c3adff65ae0184056f
Paulo202005 <paulo.argenton@gmail.com>
2021-04-11 14:15:21 -0300

    user login 1

---
+++ spec/factories/users.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+    factory :user do
+      name { "Teste" }
+      email { "user#{rand(1..1000)}@user.com" }
+      phone { "999999987" }
+      avatar { Rack::Test::UploadedFile.new(Rails.root.join('spec/support/test-avatar.png'), 'image/png') }
+      password { "123456" }
+    end
+  end
+  
