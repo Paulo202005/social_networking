ce72112831070601ee80d05e352cc7b9b7d144d1
Paulo202005 <paulo.argenton@gmail.com>
2021-04-12 18:42:49 -0300

    timeline and user details

--- config/routes.rb
+++ config/routes.rb
@@ -16,13 +16,13 @@ Rails.application.routes.draw do
     get 'potential_to_follow', to: "profile#potential_to_follow"    
     get 'following', to: "profile#following"
     get 'followers', to: "profile#followers"
-    resources :posts, only: :create
+    resources :posts, only: [:create, :destroy]
     #resources :posts, only: [:create, :destroy] do
       #member do
         #post :like_toggle
       #end
     #end
-    #resources :users, only: :show
+    resources :users, only: :show
     #resources :comments, only: [:create, :destroy]
 
     post 'follow/:id', to: "subscriptions#follow", as: :follow
